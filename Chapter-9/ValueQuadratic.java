04000734b145fb2c28ff28f4f0978c527882a392
hooperwf1 <hooperwf1@s.dcsdk12.org>
2020-09-08 10:58:38 -0600

    Quadratic formula

---
+++ Chapter-9/ValueQuadratic.java
@@ -0,0 +1,12 @@
+public class ValueQuadratic {
+
+	public static void main(String[] args){
+
+		double x = 0.66, ans, a = 3, b = -8, c = 4;
+
+		ans = (a * (x * x)) + (b * x) + c;
+
+		System.out.println("At X = " + x + " the answer is " + ans);
+	}
+
+}
