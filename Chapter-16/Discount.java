8ac6f1d62a581e00d428343d7df71f4970e14b67
hooperwf1 <hooperwf1@s.dcsdk12.org>
2020-09-17 10:43:05 -0600

    Discount OrderCheck PieEating

---
+++ Chapter-16/Discount.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class Discount {
+	public static void main(String[] args){
+		Scanner scanner = new Scanner(System.in);
+
+		System.out.print("Enter purchases: ");
+		int cents = scanner.nextInt();
+
+		cents -= cents * 0.1;
+
+		System.out.println("Discount: " + cents);
+	}	
+}
