0e7d1f2380469cf0e99c135ef0d586d57b7a40cb
hooperwf1 <hooperwf1@s.dcsdk12.org>
2020-09-29 11:57:04 -0600

    DrugPotency

---
+++ Chapter-23/DrugPotency.java
@@ -0,0 +1,21 @@
+import java.util.*;
+
+public class DrugPotency {
+	public static void main(String[] args){
+		double effective = 100;
+		int month = 0;
+		boolean isGood = true;
+		
+		while (isGood){
+			System.out.print("\nMonth: " + month + "\tEffectiveness: " + effective);
+			if(effective < 50.0){
+				isGood = false;
+			}
+			effective -= effective * 0.04;
+			month++;
+		}
+		
+		System.out.println("\tDISCARDED");
+
+	}	
+}
