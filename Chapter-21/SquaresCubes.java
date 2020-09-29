3d16b2f7ec20f1543ad75e3fa98db436cb6dc2a8
hooperwf1 <hooperwf1@s.dcsdk12.org>
2020-09-29 10:56:59 -0600

    SquaresCubes

---
+++ Chapter-21/SquaresCubes.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class SquaresCubes {
+	public static void main(String[] args){
+		Scanner scanner = new Scanner(System.in);
+
+		System.out.print("Enter N: ");
+		int n = scanner.nextInt();
+
+		int squ = 0;
+		for(int i = 1; i <= n; i++){
+			squ += i * i;
+		}
+
+		int cube = 0;
+		for(int i = 1; i <= n; i++){
+			cube += i * i * i;
+		}
+
+		System.out.println("Squares: " + squ);
+		System.out.println("Cubes: " + cube);
+	}	
+}
