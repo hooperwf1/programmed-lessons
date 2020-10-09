7ce797d7e07793a596e7b5cf55d9447ace4a71e0
hooperwf1 <hooperwf1@s.dcsdk12.org>
2020-10-09 10:26:28 -0600

    SumDivisors

---
+++ Chapter-24/SumDivisors.java
@@ -0,0 +1,26 @@
+import java.util.*;
+
+public class SumDivisors {
+
+	public static void main (String[] args) {
+
+		Scanner scanner = new Scanner(System.in);
+
+		System.out.print("Enter N: ");
+		int n = scanner.nextInt();
+
+		int sum = 0;
+		for(int i = 1; i <= (n/2.f); i++){
+			if(n % i == 0){
+				sum += i;
+			}
+		}
+
+		System.out.println("Sum: " + sum);
+		
+		if(sum == n){
+			System.out.println("Is perfect number");
+		}
+	}
+
+}
