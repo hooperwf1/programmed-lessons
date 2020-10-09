af4fef36037f96d26af093e2cba72ecc294c6591
hooperwf1 <hooperwf1@s.dcsdk12.org>
2020-10-09 10:22:09 -0600

    OddInt

--- Chapter-24/OddInt.java
+++ Chapter-24/OddInt.java
@@ -1,42 +1,20 @@
 import java.util.*;
 
-public class SnakeEyes {
+public class OddInt {
 
 	public static void main (String[] args) {
 
 		Scanner scanner = new Scanner(System.in);
-		Random random = new Random();
 
-		System.out.print("Enter times to run: ");
+		System.out.print("Enter N: ");
 		int n = scanner.nextInt();
 
-		final int max = 6;
-
-		long totalScore = 0;
-		int totalRolls = 0, moreFour = 0;
-		for (int i = 0; i < n; i++){
-
-			int roll1 = random.nextInt(max) + 1;
-			int roll2 = random.nextInt(max) + 1;
-			int currentRolls = 0;
-
-			while(roll1 + roll2 != 2){
-				totalRolls++;
-				currentRolls++;
-				totalScore += roll1 + roll2;
-				roll1 = random.nextInt(max) + 1;
-				roll2 = random.nextInt(max) + 1;
-			}
-
-			totalRolls++;
-			currentRolls++;
-
-			if(currentRolls > 4){
-				moreFour++;
-			}
+		int sum = 0;
+		for(int i = 1; i <= n; i += 2){
+			sum += i;
 		}
 
-		System.out.println("average rolls: \t" + (totalRolls/ (double) n) + "\nAverage score: \t" + (totalScore/ (double) n) + "\nPercent with more than four rolls: \t" + (moreFour/ (double) n * 100.f));
+		System.out.println("Sum: " + sum + "\tN^2: " + (n*n));
 	}
 
 }
