cdd6e175037efecfb3afab4cc51d8b7f64aec43d
hooperwf1 <hooperwf1@s.dcsdk12.org>
2020-10-12 11:50:26 -0600

    Triangle

---
+++ Chapter-25/Triangle.java
@@ -0,0 +1,22 @@
+import java.util.*;
+
+public class Triangle {
+
+	public static void main (String[] args) {
+
+		Scanner scanner = new Scanner(System.in);
+
+		System.out.print("Enter N: ");
+		int n = scanner.nextInt();
+
+		int triangle = 0;
+		for (int i = 1; i <= n; i++){
+			triangle += i;
+
+			if(Math.sqrt(triangle) % 1 == 0){
+				System.out.print(triangle + ", ");
+			}
+		}
+	}
+
+}
