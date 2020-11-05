958e05f0e9dbc95f10af68e974eb264e82c65a74
hooperwf1 <hooperwf1@s.dcsdk12.org>
2020-11-05 11:01:11 -0700

    PantryTester2.java

--- Chapter-53/PantryTester.java
+++ Chapter-53/PantryTester.java
@@ -57,6 +57,22 @@ class Pantry
   private Jam  selected ;
 
   // Constructors
+  Pantry( Jam jar1 )
+  {
+    this . jar1 = jar1 ;
+    this . jar2 = null ;
+    this . jar3 = null ;
+    selected = null ;
+  }
+
+  Pantry( Jam jar1, Jam jar2 )
+  {
+    this . jar1 = jar1 ;
+    this . jar2 = jar2 ;
+    this . jar3 = null ;
+    selected = null ;
+  }
+  
   Pantry( Jam jar1, Jam jar2, Jam jar3 )
   {
     this . jar1 = jar1 ;
@@ -64,28 +80,45 @@ class Pantry
     this . jar3 = jar3 ;
     selected = null ;
   }
-
+  
   // Methods
   public String toString()
   {
     String str = "";
-    str += "1: " +  jar1.toString()  + "\n";
-    str += "2: " +  jar2.toString()  + "\n";
-    str += "3: " +  jar3.toString()  + "\n";     
+    if(jar1 != null){
+	    str += "1: " +  jar1.toString()  + "\n";
+    }
+    
+    if(jar2 != null){
+    
+	    str += "2: " +  jar2.toString()  + "\n";
+    }
+    
+    if(jar3 != null){
+    
+	    str += "3: " +  jar3.toString()  + "\n";     
+    }
+
     return str;
   }
 
   // assume that the user entered a correct selection, 1, 2, or 3
-  public void select( int jarNumber )
+  public boolean select( int jarNumber )
   {
-    if ( jarNumber == 1 )
-      selected =  jar1 ;
+    if ( jarNumber == 1 && jar1 != null){
+      selected =  jar1;
+      return true;
+    }
+    else if ( jarNumber == 2 && jar2 != null){
+      selected = jar2;
+      return true;
+    }
+    else if(jar3 != null){
+      selected = jar3;
+      return true;
+    }
 
-    else if ( jarNumber == 2 )
-      selected = jar2 ;
-
-    else 
-      selected = jar3 ;
+    return false;
   }
 
   // spread the selected jam
